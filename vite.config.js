b347ec83f94a7ca214449c914d70751d342216df
Sıla Balkan <164004853+Silabalkan@users.noreply.github.com>
2024-12-12 17:18:42 +0300

    #3 - User Authentication (#18)
    * - setup
    
    * - setup
    
    * - setup
    
    * - setup
    
    * - setup
    
    * - setup
    
    * - setup
    
    * - delete .env
    
    * - add newlines
    
    * solved .env
    
    * test commit
    
    * user registration created
    
    * - fixed errors
    
    * registration/login are created succesfuly
    
    * - fixed errors with register; rollback previous changes because they are not needed
    
    * Remove .env file from version control
    
    * cs fix
    
    * - lint fix; added lintf to make fix command
    
    * - trying to fix .env.ci
    
    * - trying to fix .env.ci
    
    * - fix database credentials
    
    * - fix tests
    
    * - cs fix
    
    * - fix factory
    
    * The last line in the .gitignore file was removed
    
    * The .gitignore file has been fixed
    
    * Fix coding style issues
    
    * The errors on GitHub have been fixed
    
    * Fix: Addressed review comments
    
    * The code design issues have been resolved
    
    * removed empty lin
    
    * fixed empty line error
    
    * - trying to fix errors
    
    ---------
    
    Co-authored-by: EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv,  } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv  } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import { networkInterfaces } from 'os'
+import vue from '@vitejs/plugin-vue';
 
 export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
